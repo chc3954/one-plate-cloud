d6b24d9c2a0e0e55b93799434281cde772bf58a0
Hyunchul <chc3954@gmail.com>
2025-05-13 10:16:36 -0400

    [server]: Remove CORS code to avoid duplication with web server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const uploadRoute = require("./routes/uploadRoute");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/upload", uploadRoute);
 
