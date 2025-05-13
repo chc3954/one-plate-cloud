a2c22ce502534728f97df7641a289a7fdef26912
Hyunchul <chc3954@gmail.com>
2025-05-13 13:49:01 -0400

    [client]: Set the base URL to /gallery

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gallery/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ['oneplate-cloud.duckdns.org'],
